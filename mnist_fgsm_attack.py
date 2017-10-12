624fe835c696da9eeb7ed57eeb411e3f11380649
Summer Yue <yutingyue514@gmail.com>
2017-10-12 01:18:31 -0400

    Demonstrate how the epsilon indicating the extent of perturbation drastically impacts the mnist classifier's performance

--- mnist_fgsm_attack.py
+++ mnist_fgsm_attack.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from mnist import build_network, calc_loss
+from mnist import build_network, calc_loss, evaluation
 from tensorflow.examples.tutorials.mnist import input_data as mnist_data
 
 # Importing MNIST datasets
@@ -45,6 +45,32 @@ def fgsm_attack(image, label):
     perturbed_image = sess.run(perturbed_op, feed_dict={x: [image], y:[label]})
     return perturbed_image
 
+def accuracy_after_fgsm_attack(images, labels, epsilon):
+    """ Perform fast gradient sign method attack on a list of (image, label) pair to lead mnist classifier to misclassify
+    return: new accuracy after perturbation
+    """
+    tf.reset_default_graph()
+
+    # Building the graph
+    x = tf.placeholder(tf.float32, [None, input_dimension], name="input")
+    y = tf.placeholder(tf.float32, [None, output_dimension], name="labels")
+    z3, y_ = build_network(x, l1, l2, l3)
+    loss = calc_loss(z3, y)
+    accuracy = evaluation(y, y_)
+
+    #apply pertubation to images
+    pertubation = tf.sign(tf.gradients(loss, x))
+    perturbed_op = tf.squeeze(epsilon * pertubation) + images
+
+    sess=tf.Session()   
+    sess.run(tf.global_variables_initializer())
+    saver = tf.train.Saver()
+    saver.restore(sess, "./tmp/original_mnist_model-8")
+
+    perturbed_images = sess.run(perturbed_op, feed_dict={x: images, y:labels})
+    perturbed_accuracy = sess.run(accuracy, feed_dict={x: perturbed_images, y: labels})
+    return perturbed_accuracy
+
 def predict_mnist_num(image):
     """ Run the mnist model and predict on one single image
     Return: [[]] of 10 numbers indicating the probabilities from 0 - 9
@@ -86,4 +112,22 @@ def demonstrate_attack_result():
     perturbed_prediction = np.argmax(predict_mnist_num(perturbed_image), axis=1)
     print("The perturbed picture's prediction says:" + str(perturbed_prediction))
 
-demonstrate_attack_result()
+def demonstrate_attack_error_rate():
+    """ Demonstrate the FGSM attack result by showing
+    How the value of epsilon changes the error rate on the test set
+    """
+    sample_images = mnist.test.images
+    sample_labels = mnist.test.labels
+
+    epsilons = []
+    perturbed_accuracies = []
+    for epsilon in [x * 0.01 for x in range(0, 30)]:
+        epsilons.append(epsilon)
+        perturbed_accuracy = accuracy_after_fgsm_attack(sample_images, sample_labels, epsilon)
+        perturbed_accuracies.append(perturbed_accuracy)
+    plt.plot(epsilons, perturbed_accuracies)
+    plt.title('FGSM Attack: Epsilon VS Prediction Accuracy')
+    plt.show()
+
+# demonstrate_attack_result()
+demonstrate_attack_error_rate()
