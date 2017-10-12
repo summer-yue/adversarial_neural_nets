d5709df2e2fd20f511697a29780c22d7d32a1611
Summer Yue <yutingyue514@gmail.com>
2017-10-11 21:28:00 -0400

    Use saver to store the model so I can restore it later in FGSM attack

--- mnist.py
+++ mnist.py
@@ -1,8 +1,6 @@
-
 # coding: utf-8
 
 # In[1]:
-
 
 # Basic imports for libraries and the mnist data
 import tensorflow as tf
@@ -16,6 +14,11 @@ tf.set_random_seed(0)
 mnist = mnist_data.read_data_sets('MNIST_data', one_hot=True)
 input_dimension = 784
 output_dimension = 10
+learning_rate = 0.001
+batch_size = 100
+l1 = 200
+l2 = 300
+l3 = 10
 
 def visualize_original_data():
     # Visualize the original data
@@ -36,23 +39,23 @@ def build_network(x, l1_units, l2_units, l3_units):
     """
     
     # Hidden 1
-    with tf.variable_scope("layer2"):
-        w1 = tf.get_variable("w1",[input_dimension, l1_units], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
-        b1 = tf.Variable(tf.zeros([l1_units]), name="b1")
+    with tf.variable_scope("layer1"):
+        w1 = tf.get_variable("w",[input_dimension, l1_units], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+        b1 = tf.get_variable("b", [l1_units], initializer = tf.zeros_initializer())
         z1 = tf.matmul(x, w1) + b1
         y1 = tf.nn.relu(z1)
     
     # Hidden 2
     with tf.variable_scope("layer2"):
         w2 = tf.get_variable("w",[l1_units, l2_units], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
-        b2 = tf.Variable(tf.zeros([l2_units]), name="b2")
+        b2 = tf.get_variable("b", [l2_units], initializer = tf.zeros_initializer())
         z2 = tf.matmul(y1, w2) + b2
         y2 = tf.nn.relu(z2)
    
     # softmax
     with tf.variable_scope("layer3"):
-        w3 = tf.get_variable("w3",[l2_units, l3_units], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
-        b3 = tf.Variable(tf.zeros([l3_units]), name="b3")
+        w3 = tf.get_variable("w",[l2_units, l3_units], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+        b3 = tf.get_variable("b", [l3_units], initializer = tf.zeros_initializer())
         z3 = tf.matmul(y2, w3) + b3
         y_ = tf.nn.softmax(z3)
   
@@ -77,9 +80,7 @@ def training(loss, learning_rate):
     beta1=0.9
     beta2=0.999
     epsilon=1e-08
-
-    # learning_rate = tf.placeholder(tf.float32, shape=[])
-    
+ 
     optimizer = tf.train.AdamOptimizer(learning_rate, beta1=beta1, beta2=beta2, epsilon=epsilon)
     train_op = optimizer.minimize(loss)
     return train_op
@@ -91,7 +92,7 @@ def evaluation(y, y_):
     that were predicted correctly.
     """
     correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name="accuracy")
     return accuracy
 
 # Preparing for mini-batching training data
@@ -205,22 +206,19 @@ def search_hyperparameters():
         print("Optimal train accuracy:" + str(optimal_train_accuracy))
 
 
-def calculate_test_accuracy():
-    tf.reset_default_graph()
+def create_mnist_model():
+    """ Construct and train the neural net and save the model in /tmp/original_mnist_model-8
+    """
+    # Building the graph
+    x = tf.placeholder(tf.float32, [None, input_dimension], name="input")
+    y = tf.placeholder(tf.float32, [None, output_dimension], name="labels")
+    z3, y_ = build_network(x, l1, l2, l3)
+    loss = calc_loss(z3, y)
+    accuracy = evaluation(y, y_)
+    train_op = training(loss, learning_rate)
+    saver = tf.train.Saver()
     # Train MNIST classifer and evaluate the accuracy
     with tf.Session() as sess:
-        learning_rate = 0.001
-        batch_size = 100
-        l1 = 200
-        l2 = 300
-        l3 = 10
-        # Building the graph
-        x = tf.placeholder(tf.float32, [None, input_dimension], name="input")
-        y = tf.placeholder(tf.float32, [None, output_dimension], name="labels")
-        z3, y_ = build_network(x, l1, l2, l3)
-        loss = calc_loss(z3, y)
-        accuracy = evaluation(y, y_)
-        train_op = training(loss, learning_rate)
         sess.run(tf.global_variables_initializer())
 
         # mini-batch training for 8 epochs
@@ -230,8 +228,33 @@ def calculate_test_accuracy():
                 generate_mini_batches(batch_size, shuffled_train_images, shuffled_train_labels):
                     sess.run(train_op, feed_dict={x: batch_train_images, y: batch_train_labels}) 
 
-        test_accuracy = sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels})
-        print("Test accuracy is:" + str(test_accuracy))
+        # Save the variables and model to disk.
+        save_path = saver.save(sess, './tmp/original_mnist_model', global_step=8)
+        print("Model saved in file: %s" % save_path)
+        
+def calculate_test_accuracy():
+    """ Restore the saved model and calculate its test accuracy
+    """
+    tf.reset_default_graph()
 
+    # Building the graph
+    x = tf.placeholder(tf.float32, [None, input_dimension], name="input")
+    y = tf.placeholder(tf.float32, [None, output_dimension], name="labels")
+    z3, y_ = build_network(x, l1, l2, l3)
+    loss = calc_loss(z3, y)
+    accuracy = evaluation(y, y_)
+    train_op = training(loss, learning_rate)
+
+    sess=tf.Session()   
+    sess.run(tf.global_variables_initializer())
+
+    saver = tf.train.Saver()
+    saver.restore(sess, "./tmp/original_mnist_model-8")
+    print("Model restored.")
+
+    # Check the values of the variables
+    test_accuracy = sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels})
+    print("Test accuracy is:" + str(test_accuracy))
+
+create_mnist_model()
 calculate_test_accuracy()
-
