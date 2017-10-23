6dd6c24effd3c8336312ace8ac06879193bc728c
Summer Yue <yutingyue514@gmail.com>
2017-10-23 14:30:15 -0400

    Regularization would make a neural net more resistant

---
+++ structures_vs_robustness/regularization/mnist.py
@@ -0,0 +1,193 @@
+# coding: utf-8
+
+# In[1]:
+
+# Basic imports for libraries and the mnist data
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data as mnist_data
+
+tf.set_random_seed(0)
+
+# Download images and labels into mnist.test (10K images+labels) and mnist.train (60K images+l/Users/summer/Documents/Research/adversarial_neural_nets/mnist.pyabels)
+mnist = mnist_data.read_data_sets('MNIST_data', one_hot=True)
+input_dimension = 784
+output_dimension = 10
+learning_rate = 0.001
+batch_size = 100
+l1 = 200
+l2 = 300
+l3 = 10
+
+def visualize_original_data():
+    # Visualize the original data
+    index = 1
+    print(mnist.train.images.shape)
+    print(mnist.train.labels.shape)
+
+    plt.imshow(mnist.train.images[index, :].reshape(28, 28))
+    plt.show()
+    print ("y = " + str(mnist.train.labels[index, :]))
+
+def build_network(x, l1_units, l2_units, l3_units):
+    """Build the MNIST model with 2 hidden layers and one linear layer.
+    params: 
+        x: input placeholder
+    Returns: 
+        Output tensor with the computed logits
+    """
+    
+    # Hidden 1
+    with tf.variable_scope("layer1"):
+        w1 = tf.get_variable("w",[input_dimension, l1_units], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+        b1 = tf.get_variable("b", [l1_units], initializer = tf.zeros_initializer())
+        z1 = tf.matmul(x, w1) + b1
+        y1 = tf.nn.relu(z1)
+    
+    # Hidden 2
+    with tf.variable_scope("layer2"):
+        w2 = tf.get_variable("w",[l1_units, l2_units], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+        b2 = tf.get_variable("b", [l2_units], initializer = tf.zeros_initializer())
+        z2 = tf.matmul(y1, w2) + b2
+        y2 = tf.nn.relu(z2)
+   
+    # softmax
+    with tf.variable_scope("layer3"):
+        w3 = tf.get_variable("w",[l2_units, l3_units], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+        b3 = tf.get_variable("b", [l3_units], initializer = tf.zeros_initializer())
+        z3 = tf.matmul(y2, w3) + b3
+        y_ = tf.nn.softmax(z3)
+  
+    weights = {}
+    weights["w1"] = w1
+    weights["w2"] = w2
+    weights["w3"] = w3
+
+    return z3, y_, weights # logits and probilities
+
+def calc_loss(z3, y, weights):
+    """Calculates the loss from the logits and the labels.
+    params:
+        z3: logits for output tensor from build_network
+        y: labeled data indicating the numeric value of the picture, [None, 10]
+    """
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=z3, labels=y)
+   
+    reg_constant = 0.001
+    l2_loss = reg_constant * (tf.nn.l2_loss(weights["w1"]) + 
+                            tf.nn.l2_loss(weights["w2"]) +
+                            tf.nn.l2_loss(weights["w3"])) 
+
+    loss = tf.reduce_mean(cross_entropy + l2_loss)
+    return loss
+
+def training(loss, learning_rate):
+    """Sets up the training Ops.
+    params:
+        loss: cross entropy loss
+        learning_rate
+    """
+    beta1=0.9
+    beta2=0.999
+    epsilon=1e-08
+ 
+    optimizer = tf.train.AdamOptimizer(learning_rate, beta1=beta1, beta2=beta2, epsilon=epsilon)
+    train_op = optimizer.minimize(loss)
+    return train_op
+
+def evaluation(y, y_):
+    """Evaluate the quality of the logits at predicting the label.
+    Returns:
+    A scalar int32 tensor with the number of examples (out of batch_size)
+    that were predicted correctly.
+    """
+    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name="accuracy")
+    return accuracy
+
+# Preparing for mini-batching training data
+def generate_shuffled_train_data():
+    """ Shuffle the images and labels in the training set in unison
+    Return:
+        shuffled train images and train labels
+    """
+    assert len(mnist.train.images) == len(mnist.train.labels)
+    p = np.random.permutation(len(mnist.train.labels))
+    shuffled_train_images = mnist.train.images[p]
+    shuffled_train_labels = mnist.train.labels[p]
+    return shuffled_train_images, shuffled_train_labels
+
+
+def generate_mini_batches(batch_size, train_images, train_labels):
+    """ Yield mini batches in tuples from the original dataset with a specified batch size
+    Params: 
+        batch_size: number of training data in a sample
+        train_images: all of train images [None, 784] after shuffling
+        train_labels: all of train labels [None, 10] after shuffling
+    Return:
+        A generator yielding each mini batch([batch_num, 784], [batch_num, 10])
+    Notes:
+        the last data not divisible by mini-batch is thrown away
+    """
+    train_image_num = len(train_labels)
+    for i in range(int(train_image_num / batch_size)):
+        start_slice_index = i * batch_size
+        end_slice_index = (i + 1) * batch_size
+        yield (train_images[start_slice_index:end_slice_index],
+               train_labels[start_slice_index:end_slice_index])
+
+def create_mnist_model():
+    """ Construct and train the neural net and save the model in /tmp/original_mnist_model-8
+    """
+    # Building the graph
+    x = tf.placeholder(tf.float32, [None, input_dimension], name="input")
+    y = tf.placeholder(tf.float32, [None, output_dimension], name="labels")
+    z3, y_, weights = build_network(x, l1, l2, l3)
+    loss = calc_loss(z3, y, weights)
+    accuracy = evaluation(y, y_)
+    train_op = training(loss, learning_rate)
+    saver = tf.train.Saver()
+    # Train MNIST classifer and evaluate the accuracy
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        # mini-batch training for 8 epochs
+        for epoch in range(8): # This is the optimal number of epochs after tuning
+            shuffled_train_images, shuffled_train_labels = generate_shuffled_train_data()
+            for (batch_train_images, batch_train_labels) in\
+                generate_mini_batches(batch_size, shuffled_train_images, shuffled_train_labels):
+                    sess.run(train_op, feed_dict={x: batch_train_images, y: batch_train_labels}) 
+
+        # Save the variables and model to disk.
+        save_path = saver.save(sess, './tmp/l2_regularized_mnist_model', global_step=8)
+        print("Model saved in file: %s" % save_path)
+        
+def calculate_test_accuracy():
+    """ Restore the saved model and calculate its test accuracy
+    """
+    tf.reset_default_graph()
+
+    # Building the graph
+    x = tf.placeholder(tf.float32, [None, input_dimension], name="input")
+    y = tf.placeholder(tf.float32, [None, output_dimension], name="labels")
+    z3, y_, weights = build_network(x, l1, l2, l3)
+    loss = calc_loss(z3, y, weights)
+    accuracy = evaluation(y, y_)
+    train_op = training(loss, learning_rate)
+
+    sess=tf.Session()   
+    sess.run(tf.global_variables_initializer())
+
+    saver = tf.train.Saver()
+    saver.restore(sess, "./tmp/l2_regularized_mnist_model-8")
+    print("Model restored.")
+
+    # Check the values of the variables
+    train_accuracy = sess.run(accuracy, feed_dict={x: mnist.train.images, y: mnist.train.labels})
+    print("Train accuracy is:" + str(train_accuracy))
+    test_accuracy = sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels})
+    print("Test accuracy is:" + str(test_accuracy))
+
+#create_mnist_model()
+calculate_test_accuracy()
